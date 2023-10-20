b42151d96a9117cd2e68d46e9e2d9caa4d38d38c
Lucas Hansen <lucas.hansenst@gmail.com>
2023-10-19 20:49:47 -0500

    refactored fetch

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
 
 const searchButton = document.getElementById('searchButton');
-        const movieSearch = document.getElementById('movieSearch');
-        const movieResults = document.getElementById('movieResults');
-        
+const movieSearch = document.getElementById('movieSearch');
+const movieResults = document.getElementById('movieResults');
 
-        searchButton.addEventListener('click', () => {
-
+function searchHandler(event) {
     const apiKey = '820b9eeb';
-
-
-    fetch(`https://www.omdbapi.com/?s=${movieSearch.value}&apikey=${apiKey}`)
+    let value
+    console.log(event.target);
+    if(event.target.matches(".bi-search") || event.target.matches("#searchButton")) {
+        value = movieSearch.value;
+    } else {
+        value = event;
+    }
+    
+    fetch(`https://www.omdbapi.com/?s=${value}&apikey=${apiKey}`)
         .then(response => response.json())
         .then(data => {
             if (data.Response === "True") {
                 console.log(data);
 
-                        movieResults.innerHTML = '';
-                        
-                        data.Search.forEach(movie => {
-                        const movieElement = document.createElement('div');
-                        movieElement.setAttribute("class", "col-4");
-                         movieElement.innerHTML = `
+                movieResults.innerHTML = '';
+
+                data.Search.forEach(movie => {
+                    const movieElement = document.createElement('div');
+                    movieElement.setAttribute("class", "col-4");
+                    movieElement.innerHTML = `
                          <div class="card mt-5">
                          <div class="card-body">
                          <h5 class="card-title">${movie.Title}</h5>
                              <img class="card-img-top" src="${movie.Poster}" alt="${movie.Title} Poster">
-                             <p class="card-text">${movie.Year}</p>
-                             <p class="card-text">${movie.Type}</p>
+                             <p class="card-text">${movie.Year} ${movie.Type} directed by ${movie.Director}</p>
+                             <button class="card-button">See More</button>
                            </div>
                          </div>
                        </div>`;
@@ -40,7 +44,17 @@ const searchButton = document.getElementById('searchButton');
         .catch(error => {
             console.error('Error:', error);
         });
-});
+}
+
+searchButton.addEventListener('click', searchHandler);
+
+function movieDetail(event) {
+    if (event.target.matches("button")) {
+        console.dir(event.target.previousElementSibling.previousElementSibling.previousElementSibling.textContent);
+    }
+}
 
 localStorage.setItem("movieSearch", "Hulk");
 document.getElementById("movieSearch").innerHTML = localStorage.getItem("movieSearch");
+
+movieResults.addEventListener("click", movieDetail);
